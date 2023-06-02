d35de3f9d9da6bf64200be22f66084c2910c75fa
Kurt Neinstedt <kurtcneinstedt>
2023-06-02 15:22:21 -0400

    First successful run with hard-coded filepaths

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import arcpy as ap
 import arcpy.management as am
 import os
 
-target_folder = r'C:\GEOG485\Lesson2\Lesson2Data'
-template_shapefile = ap.GetParameterAsText()
+target_folder = r'C:\GEOG485\Lesson2\Lesson2Data\CountyLines.shp'
+template_shapefile = r'C:\GEOG485\Lesson2\Lesson2Data\CityBoundaries.shp'  # ap.GetParameterAsText(1)
 
 
-def reproject_in_place(in_shapefile, desired_spatial_ref):
+def reproject_in_place(in_shapefile, template_shapefile):
     try:
         am.Project(in_shapefile,
                    os.path.join(in_shapefile, "_projected"),
@@ -19,15 +19,19 @@ def reproject_in_place(in_shapefile, desired_spatial_ref):
                    None,
                    ap.SpatialReference(in_shapefile)
                    )
-    except:
-        pass
+    except TypeError:
+        print("TypeError: Check arguments.")
 
 
-def main(folder, shapefile):
-    template_reference = ap.SpatialReference(template_shapefile)
-    for file in os.listdir(folder):
-        reproject_in_place(file, template_reference)
+def main(file, shapefile):
+    print(ap.SpatialReference(shapefile))
+    template_reference = ap.SpatialReference(shapefile)
+    reproject_in_place(file, template_reference)
 
 
 if __name__ == '__main__':
-    main(target_folder, template_shapefile)
+    #main(target_folder, template_shapefile)
+    am.Project(target_folder,
+               os.path.join(r'C:\GEOG485\Lesson2\Lesson2Data\CountyLines_projected.shp'),
+               ap.Describe(template_shapefile).spatialReference,
+               None)
